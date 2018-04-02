7367eb8cdb0ed21fcd48f49bbbf1ca08695e6dcf
Sodanheang <vandysodanheang@gmail.com>
2018-04-02 11:35:13 +0700

    Refactor Version

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
   has_many :chat_rooms, dependent: :destroy
   has_many :messages, dependent: :destroy
