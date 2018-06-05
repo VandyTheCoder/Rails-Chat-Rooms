301b43279d012d7450ac06044616b4cf5a588077
Sodanheang <vandysodanheang@gmail.com>
2018-06-05 21:06:05 +0700

    Bug View

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -2,6 +2,6 @@ class ChatRoom < ApplicationRecord
   belongs_to :user
   has_many :messages, dependent: :destroy
 
-  validates :title, presence: true
-  validates :description, length: {minimum: 5, maximum: 120}, allow_blank: true
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 100}
+  validates :description, length: {minimum: 5, maximum: 1000}, allow_blank: true
 end
